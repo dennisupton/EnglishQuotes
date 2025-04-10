21c96e3fd1ec6f9ab5128377235965d864ab17c9
Audey <audeygodfrey@gmail.com>
2025-04-11 00:00:32 +0100

    Disable scrolling on test page

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var counter = 0;  // Track completed quotes
 var url = "quotebooks/quotes_";
 let quotes = [], names = [];
 
+window.onscroll = function () {
+    window.scrollTo(0,0);
+}
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
